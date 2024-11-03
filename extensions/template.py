f8e5d943d9cd042d795392193de89113a7f92dc5
CheesiestMaster <jason@jdsnetwork.com>
2024-11-03 06:11:27 -0500

    corrected issues in template

--- extensions/template.py
+++ extensions/template.py
@@ -1,13 +1,13 @@
 from logging import getLogger
-from discord.ext.commands import Cog
-from discord import Client
+from discord.ext.commands import GroupCog, Bot
+from discord import Interaction, app_commands as ac
 logger = getLogger(__name__)
 
-class CogClass(Cog):
+class CogClass(GroupCog):
     pass # this is a template file, so no code is needed here
 
-bot: Client = None
-async def setup(_bot: Client):
+bot: Bot = None
+async def setup(_bot: Bot):
     global bot
     bot = _bot
     await bot.add_cog(CogClass(bot))
